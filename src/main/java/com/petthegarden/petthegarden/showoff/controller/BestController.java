168457162e4d9f4600799a766f5ec839dc0ebca5
halland09 <pjs980720@gmail.com>
2025-07-02 17:26:52 +0900

    jjaphyeongwook sajin chuga

--- src/main/java/com/petthegarden/petthegarden/showoff/controller/BestController.java
+++ src/main/java/com/petthegarden/petthegarden/showoff/controller/BestController.java
@@ -1,4 +1,13 @@
 package com.petthegarden.petthegarden.showoff.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BestController {
+    @GetMapping("/showoff/best")
+    public String showShowoffPage() {
+        // correct.html을 템플릿으로 사용
+        return "showoff/best";
+    }
 }
