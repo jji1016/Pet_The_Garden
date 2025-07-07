e15c9d51eb9b25a5f4a5fd4810572ab8f02a210a
jji1016 <joeyji1016@gmail.com>
2025-07-07 17:44:42 +0900

    .

--- src/main/java/com/petthegarden/petthegarden/stray/StrayController.java
+++ src/main/java/com/petthegarden/petthegarden/stray/StrayController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,16 +21,23 @@ public class StrayController {
 
 
     @GetMapping("/protect")
-    public String protect(Model model) {
+    public String protect(@RequestParam(required = false) String startDate,
+                          @RequestParam(required = false) String endDate,
+                          @RequestParam(required = false) String discvryPlc,
+                          @RequestParam(required = false) String species,
+                          Model model) {
 //        strayService.deleteAllInBatch(); // 기존 데이터 전부 삭제
 //        strayService.refreshData(); //새로운 데이터 db에 저장
 
-        // 서비스에서 API 호출해 데이터 받아오기
+        // 서비스에서 API 호출해 데이터 받아오기 //이거 검색결과 기준으로 수정해야됨(서비스에 새롭게 만들기)
         List<StrayDto> strayList = strayApiService.fetchStrayData();
         log.info("strayList ===={}", strayList.toString());
 
-        // 모델에 담아서 뷰에 넘기기
         model.addAttribute("strayList", strayList);
+        model.addAttribute("startDate", startDate);
+        model.addAttribute("endDate", endDate);
+        model.addAttribute("discvryPlc", discvryPlc);
+        model.addAttribute("species", species);
 
         return "stray/protect";
     }
