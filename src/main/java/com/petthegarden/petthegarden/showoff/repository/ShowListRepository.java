d2b4f22bf61d41296312f31c2885d22b3c9a8b2f
halland09 <pjs980720@gmail.com>
2025-07-08 11:26:09 +0900

    coshort image chuga

--- src/main/java/com/petthegarden/petthegarden/showoff/repository/ShowListRepository.java
+++ src/main/java/com/petthegarden/petthegarden/showoff/repository/ShowListRepository.java
@@ -1,8 +1,15 @@
-package com.petthegarden.petthegarden.showoff.repository;// ShowListRepository.java
+package com.petthegarden.petthegarden.showoff.repository;
+
 import com.petthegarden.petthegarden.entity.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ShowListRepository extends JpaRepository<Pet, Integer> {
     List<Pet> findTop3ByOrderByPetLikeDesc();
+    Page<Pet> findByIdNotIn(List<Integer> ids, Pageable pageable);
 }
+
+
+
