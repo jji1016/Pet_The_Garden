ef2d450329afd4aea1cd6ac050812559f99b3f63
gyeongseon308 <zoon308@gmail.com>
2025-07-09 17:36:09 +0900

    regdate 들어가도록 수정

--- src/main/java/com/petthegarden/petthegarden/PetTheGardenProjectApplication.java
+++ src/main/java/com/petthegarden/petthegarden/PetTheGardenProjectApplication.java
@@ -2,10 +2,12 @@ package com.petthegarden.petthegarden;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing //이게 있어야 regdate가 자동으로 들어감
 public class PetTheGardenProjectApplication {
 
     public static void main(String[] args) {
