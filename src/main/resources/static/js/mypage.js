2a3502ee553b578abd92f8526a275796dfddcba6
gyeongseon308 <zoon308@gmail.com>
2025-07-10 11:00:11 +0900

    마이페이지 페이지별 이동 수정

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,22 +1,29 @@
+const tabMap = {
+    profile: 0,
+    talent: 1,
+    freeboard: 2,
+    pet: 3,
+    follow: 4,
+    qna: 5
+};
+
 $(document).ready(function () {
-    $(".list1>li").removeClass("l_on");
-    $(".list2>li").removeClass("l_on");
-    $(".list1>li").eq(0).addClass("l_on");
+    // 현재 탭 파악 (URL에서)
+    const urlParams = new URLSearchParams(window.location.search);
+    const activeTab = urlParams.get("tab") || "profile";
+    const index = tabMap[activeTab] || 0;
 
-    $(".list1>li, .list2>li").click(function () {
-        $(".list1>li, .list2>li").removeClass("l_on");
+    $(".list1 > li").removeClass("l_on").eq(index).addClass("l_on");
+    $(".content").hide().eq(index).show();
+
+    // 탭 클릭 시
+    $(".list1 > li").click(function () {
+        const clickedIndex = $(this).index();
+        $(".list1 > li").removeClass("l_on");
         $(this).addClass("l_on");
-    });
 
-    $(".list1>li").click(function () {
-        let index = $(this).index();
-        $(".content").hide();
-        $(".content").eq(index).show();
-    });
-
-    $(".list2>li").click(function () {
-        let index = $(this).index();
-        $(".content").hide();
-        $(".content").eq(index + 3).show();
+        $(".content").hide().eq(clickedIndex).show();
+        history.replaceState(null, "", "?tab=" + Object.keys(tabMap)[clickedIndex]);
+        // URL은 바뀌지만 새로고침 없이 처리됨
     });
 });
