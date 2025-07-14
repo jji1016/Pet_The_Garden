d67b44db76deeb3f9665207db02ab8520704d223
halland09 <pjs980720@gmail.com>
2025-07-14 14:48:19 +0900

    company,faq finished

--- src/main/java/com/petthegarden/petthegarden/community/CompanyController.java
+++ src/main/java/com/petthegarden/petthegarden/community/CompanyController.java
@@ -2,12 +2,14 @@ package com.petthegarden.petthegarden.community;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/community")
 public class CompanyController {
-    @GetMapping("/community/company")
-    public String showCommunityPage() {
-        // company.html을 템플릿으로 사용
-        return "community/company";
+
+    @GetMapping("/company")
+    public String company() {
+        return "community/company"; // Thymeleaf template path
     }
 }
